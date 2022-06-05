e4efed5be9dbd3acda2289b955b36b8c4f658bbb
youbaidjis <youba.etud@gmail.com>
2022-06-05 12:18:50 -0700

    Completed Assignment

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,26 @@
 import React from "react";
+import Links from "./Links";
 
-function About() {
+function condition (props){
+  const bio = props.bio
+  if (!bio){
+   return null
+  }
+  {    
+     return <p>{bio}</p>
+    }
+  }
+
+
+
+function About(props) {
+  
   return (
     <div id="about">
-      <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      <h2>About Me</h2>   
+      {condition(props)}   
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
+      <Links github={props.links.github} linkedin={props.links.linkedin} />
     </div>
   );
 }
